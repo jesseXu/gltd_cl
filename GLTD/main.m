9027b4a7249364854d244ab0eecac9933033b262
Jesse Xu <jesse0628@gmail.com>
2012-03-30 23:49:47 +0800

    first commit

---
+++ GLTD/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GLTD
+//
+//  Created by Jesse xu on 12-3-30.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
